32f0c92cb9f415db93d7113b7c06a80ab2ec06a1
Daniel <Daniel@LAPTOP-FMH5E13S>
2018-02-06 17:49:16 +0100

    Created UI Menu

--- Labb 4 ConsolAPP/DataContext.cs
+++ Labb 4 ConsolAPP/DataContext.cs
@@ -4,16 +4,47 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Labb_4_ConsolAPP
 {
-    public static class DataContext
+    public class DataContext
     {
-        private static void MessageToUser(string message)
+        static CosmoDB cosmoDB = new CosmoDB();
+        public static void Menu()
         {
-            Console.WriteLine(message);
+            Console.WriteLine("Welcome to Azure-Cosmos Labb!");
+            Thread.Sleep(1000);
+
+            ConsoleKey key;
+            do
+            {
+
+                Console.WriteLine("1.) Add User\n2.) Show all users\n3.) Show pending pictures");
+                key = Console.ReadKey(true).Key;
+                switch(key)
+                {
+                    case ConsoleKey.D1:
+                        // Fråga efter email och photo och lägg sedan till user!
+                        cosmoDB.CreateDocuments();
+                        break;
+                    case ConsoleKey.D2:
+                        cosmoDB.ExecuteSimpleQuery();
+                        break;
+                    case ConsoleKey.D3:
+                        cosmoDB.ExecuteSimpleQuery();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input. Try again!");
+                        Console.ReadKey();
+                        break;
+            
+                }
+
+            } while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key != ConsoleKey.D3);
         }
+
 
         private static bool IsValid(string input)
         {
